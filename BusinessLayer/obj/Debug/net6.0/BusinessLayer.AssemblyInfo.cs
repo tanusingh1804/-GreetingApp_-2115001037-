fbd3855802e06858c4273f8279351f19ef9a068b
tanusingh1804 <tanusingh1874@gmail.com>
2025-03-05 14:57:57 +0530

    Saved local changes before merging UC4

--- BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
+++ BusinessLayer/obj/Debug/net6.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94123cda6f0774591c6fd19fba6f16900c38d05e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9054db79b108a0e67cd8a62bda7ca950fb6c2d0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
